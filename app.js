55ef80521facba379ce0bf3eb0eef2edd03b322c
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-12-20 11:22:43 +0000

    work complete

---
+++ app.js
@@ -0,0 +1,167 @@
+
+
+
+
+
+let arr = [
+    {
+        brand: "Samsung",
+        model: "SamsungA30",
+        price: "40000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA10",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA20",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY20",
+        price: "20000",
+        camera: "40px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY11",
+        price: "20000",
+        camera: "40px",
+        ram: "3gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY15",
+        price: "30000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "64gb",
+        
+    },
+    {
+        brand: "Motorola",
+        model: "Motorola123",
+        price: "5000",
+        camera: "2px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone12",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone13",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "IphoneX",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+];
+
+
+console.log(arr);
+
+
+const user = document.getElementById("user_mobile");
+
+const user1=arr.filter((user_2,user_3)=>{
+
+    
+user.innerHTML+=`
+<ul>
+            <li>${user_2.brand}brand</li>
+            <li>${user_2.model}model</li>
+            <li>${user_2.price}price</li>
+            <li>${user_2.camera}camera</li>
+            <li>${user_2.ram}ram</li>
+            <li>${user_2.rom}rom</li>
+        </ul>`
+
+
+},
+);
+const inp = document.getElementById("inputt").value
+
+console.log(inp);
+
+function hamzahh(){
+
+const dadda = document.getElementById("distance")
+dadda.innerHTML+=
+
+
+`  
+
+
+`
+console.log(dadda);
+}
+hamzahh();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
